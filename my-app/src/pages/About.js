0cdde23bc3dcdbd013f826c6ed4671dd908c0583
Noah Fontenette <noahfontenette@outlook.com>
2022-12-06 20:44:47 -0600

    0.0.007

--- my-app/src/pages/About.js
+++ my-app/src/pages/About.js
@@ -5,7 +5,7 @@ const About = () => {
         <div className='container-about'>
             <div className='container-info'>
                 <h1 className='about-title'>About Me</h1>
-                <p className='home-stitle'>
+                <p className='about-stitle'>
                     Hello, my name is Noah Fontenette. I am a aspiring frontend developer and designer. I have experince using HTML, CSS, Javascript, and React.
                     <br />Using HTML, CSS, Javascript and React, I have worked with mulple clients, and grew as a developer and designer throughout.
                     <br />To work with me today contact me below.
--- my-app/src/pages/Projects.js
+++ my-app/src/pages/Projects.js
@@ -4,43 +4,51 @@ const Projects = () => {
     return (
         <div className="container-proj">
             <h1 className="proj-title">Projects</h1>
-            <p className="home-stitle">Collection of recent Frontend Development and Desing projects.</p>
-            <a href="/">
-                <div className="project">
-                    <h3 className="pb-title">Unknown project</h3>
-                    <p className="pb-description">Unknown project description.</p>
-                </div>
-            </a>
-            <a href="/">
-                <div className="project">
-                    <h3 className="pb-title">Unknown project</h3>
-                    <p className="pb-description">Unknown project description.</p>
-                </div>
-            </a>
-            <a href="/">
-                <div className="project">
-                    <h3 className="pb-title">Unknown project</h3>
-                    <p className="pb-description">Unknown project description.</p>
-                </div>
-            </a>
-            <a href="/">
-                <div className="project">
-                    <h3 className="pb-title">Unknown project</h3>
-                    <p className="pb-description">Unknown project description.</p>
-                </div>
-            </a>
-            <a href="/">
-                <div className="project">
-                    <h3 className="pb-title">Unknown project</h3>
-                    <p className="pb-description">Unknown project description.</p>
-                </div>
-            </a>
-            <a href="/">
-                <div className="project">
-                    <h3 className="pb-title">Unknown project</h3>
-                    <p className="pb-description">Unknown project description.</p>
-                </div>
-            </a>
+            <p className="proj-stitle">Collection of recent Frontend Development and Design projects.</p>
+            <div className="projects">
+                <a href="/" className="project">
+                    <div>
+                        <h3 className="pb-title">Unknown project</h3>
+                        <p className="pb-description">Unknown project description.</p>
+                        <img src='https://images.unsplash.com/photo-1670293045842-f35707741a0d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1015&q=80' alt="" className='pbox-img'/>
+                    </div>
+                </a>
+                <a href="/" className="project">
+                    <div>
+                        <h3 className="pb-title">Unknown project</h3>
+                        <p className="pb-description">Unknown project description.</p>
+                        <img src='https://images.unsplash.com/photo-1670331524356-8a54cf7d2064?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2233&q=80' alt="" className='pbox-img'/>
+                    </div>
+                </a>
+                <a href="/" className="project">
+                    <div>
+                        <h3 className="pb-title">Unknown project</h3>
+                        <p className="pb-description">Unknown project description.</p>
+                        <img src='https://images.unsplash.com/photo-1666190486038-945cf8ad9856?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=988&q=80' alt="" className='pbox-img'/>
+                    </div>
+                </a>
+                <a href="/" className="project">
+                    <div>
+                        <h3 className="pb-title">Unknown project</h3>
+                        <p className="pb-description">Unknown project description.</p>
+                        <img src='https://images.unsplash.com/photo-1670115654868-47a2ac2cc597?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80' alt='' className='pbox-img' />
+                    </div>
+                </a>
+                <a href="/" className="project">
+                    <div>
+                        <h3 className="pb-title">Unknown project</h3>
+                        <p className="pb-description">Unknown project description.</p>
+                        <img src='https://images.unsplash.com/photo-1669436024664-dd5df8cbffe2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80' alt='' className='pbox-img' />
+                    </div>
+                </a>
+                <a href="/" className="project">
+                    <div>
+                        <h3 className="pb-title">Unknown project</h3>
+                        <p className="pb-description">Unknown project description.</p>
+                        <img src='https://images.unsplash.com/photo-1668808345879-2711d41cf70f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80' alt='' className='pbox-img' />
+                    </div>
+                </a>
+            </div>
         </div>
     );
 }
